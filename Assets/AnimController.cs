41573961995e1cc7ea211ab2c492905225f5f28a
Domagoj Hamzić <domagoj.hamzic@gdi.net>
2021-05-03 19:43:37 +0200

    Dodana provjera je li pogled usmjeren prema određenom objektu.
    Trenutno se samo ispisuje u Debug-u

--- Assets/AnimController.cs
+++ Assets/AnimController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimController : MonoBehaviour
 {
     public Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/SelectionManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    //[SerializeField] private Material highlightMaterial;
+    //[SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null) {
+            Debug.Log("Interakcija gotova");
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                _selection = selection;
+                Debug.Log("Interakcija");
+            }
+        }
+    }
+}
