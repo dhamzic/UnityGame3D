dde1192a094163c081b1025a0fd2cedea7acf149
Domagoj Hamzić <domagoj.hamzic@gdi.net>
2021-05-03 19:12:37 +0200

    Dodan objekt iz Blendera i postavljena animacija otvaranja ladice pritiskom na tipku 1

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 10, 10), "");
+    }
+}
+
