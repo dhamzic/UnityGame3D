c9c495d8dd723055aeacd2a8d61954c8a86ddd41
Domagoj Hamzić <domagoj.hamzic@gdi.net>
2021-05-07 10:38:08 +0200

    Uređeni folder-i.
    Napravljen uredan raspored korištenih objekata.

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -23,7 +23,6 @@ public class SelectionManager : MonoBehaviour
         {
             var selectionRenderer = _selection.GetComponent<Renderer>();
             selectionRenderer.material = defaultMaterial;
-            Debug.Log("Interakcija gotova");
             _selection = null;
         }
 
@@ -43,7 +42,7 @@ public class SelectionManager : MonoBehaviour
                         selectionRenderer.material = highlightMaterial;
                     }
                     _selection = selection;
-                    Debug.Log("Interakcija " + hit.distance + " m");
+                    Debug.Log("Interakcija sa " + hit.collider.gameObject.name);
                 }
             }
         }
