6989f50b04f79a2019a3280c7019321b96a8034f
Domagoj Hamzić <domagoj.hamzic@gdi.net>
2021-05-09 18:19:12 +0200

    Pojavljivanje teksta za manipulaciju nakon highlight-anja na objekt ladice.

---
+++ Assets/Scripts/DrawerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerController : MonoBehaviour
+{
+    public GameObject OpenCloseTextParent;
+    public bool PrefabInstantiated = false;
+
+    GameObject instantiatedPrefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ShowFloatingText()
+    {
+        if (PrefabInstantiated == false)
+        {
+            instantiatedPrefab = Instantiate(OpenCloseTextParent);
+            PrefabInstantiated = true;
+            Debug.Log("INSTANTIATE");
+        }
+
+        if (PrefabInstantiated == true)
+        {
+            if (OpenCloseTextParent.activeInHierarchy == false)
+            {
+                instantiatedPrefab.SetActive(true);
+            }
+        }
+    }
+    public void DeleteFloatingText()
+    {
+        if (instantiatedPrefab != null)
+        {
+            instantiatedPrefab.SetActive(false);
+        }
+    }
+
+}
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -8,28 +8,40 @@ public class SelectionManager : MonoBehaviour
     [SerializeField] private Material highlightMaterial;
     [SerializeField] private Material defaultMaterial;
 
+    //Provjera je li trenutno miš preko objekta koji služi za interakciju
+    bool isHovering = false;
+
+    //Provjera je li PREFAB uključen
+    bool objectInfoTurnedOn = false;
+
     public AnimController animationScript;
+    private DrawerController drawerScript;
 
     private Transform _selection;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        drawerScript = GetComponent<DrawerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isHovering == false)
+        {
+            drawerScript.DeleteFloatingText();
+            objectInfoTurnedOn = false;
+        }
+
         if (_selection != null)
         {
             var selectionRenderer = _selection.GetComponent<Renderer>();
             selectionRenderer.material = defaultMaterial;
-            Debug.Log("Interakcija gotova");
             _selection = null;
         }
 
-        //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         var ray = Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
@@ -39,20 +51,42 @@ public class SelectionManager : MonoBehaviour
                 var selection = hit.transform;
                 if (selection.CompareTag(selectableTag))
                 {
+                    isHovering = true;
                     var selectionRenderer = selection.GetComponent<Renderer>();
                     if (selectionRenderer != null)
                     {
                         selectionRenderer.material = highlightMaterial;
                     }
                     _selection = selection;
-                    Debug.Log("Interakcija " + hit.transform.name);
+
+
+                    if (objectInfoTurnedOn == false)
+                    {
+                        drawerScript.ShowFloatingText();
+                    }
+                    objectInfoTurnedOn = true;
+
+
                     if (Input.GetKeyDown("e"))
                     {
                         animationScript.StartAnimation(hit.transform.name);
                     }
 
                 }
+                else
+                {
+                    isHovering = false;
+                }
             }
+            else
+            {
+                isHovering = false;
+
+            }
+        }
+        else
+        {
+            isHovering = false;
         }
     }
 }
