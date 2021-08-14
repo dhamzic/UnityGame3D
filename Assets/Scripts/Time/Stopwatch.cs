0b51834a4c6387d987299c9272b1baccee5f86b7
dhamzic <dhamzic797@gmail.com>
2021-08-14 13:38:53 +0200

    Kreirana štoperica

---
+++ Assets/Scripts/Time/Stopwatch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Stopwatch : MonoBehaviour
+{
+    bool timerActive = false;
+    float currentTime;
+    public Text currentTimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sekunde
+        currentTime = 0;
+        StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive == true)
+        {
+            //-Time.deltaTime jer 60fps nekad zna biti varijabilan zbog rada CPU-a
+            currentTime = currentTime + Time.deltaTime;
+        }
+
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+    }
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+    public void StopTimer()
+    {
+        timerActive = false;
+    }
+}
---
+++ Assets/Scripts/Time/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    bool timerActive = false;
+    float currentTime;
+    public int startMinutes;
+    public Text currentTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sekunde
+        currentTime = startMinutes * 60;
+        StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive == true)
+        {
+            //-Time.deltaTime jer 60fps nekad zna biti varijabilan zbog rada CPU-a
+            currentTime = currentTime - Time.deltaTime;
+            if (currentTime <= 0) {
+                StopTimer();
+            }
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+    }
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+    public void StopTimer()
+    {
+        timerActive = false;
+    }
+}
